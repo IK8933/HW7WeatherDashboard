5e41db403656f828ff801e10f762b9779c6bb4dd
IK8933 <iankessack1989@gmail.com>
2024-12-29 20:55:33 -0500

    working code

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -1,22 +1,20 @@
+// Import the routes
+// TODO: Serve static files of entire client dist folder
+// TODO: Implement middleware for parsing JSON and urlencoded form data
+// TODO: Implement middleware to connect the routes
+// Start the server on the port
+// Import the routes
 import dotenv from 'dotenv';
 import express from 'express';
+import routes from './routes/index.js';
 dotenv.config();
 
-// Import the routes
-import routes from './routes/index.js';
-
 const app = express();
-
 const PORT = process.env.PORT || 3001;
 
-// Serve static files of entire client dist folder
 app.use(express.static('../client/dist'));
-
-// Implement middleware for parsing JSON and urlencoded form data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Implement middleware to connect the routes
 app.use(routes);
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
